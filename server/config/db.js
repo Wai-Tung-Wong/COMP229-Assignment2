d88c5550fca3fbf0edfc51495881c72cf9945a36
wwong174@my.centennialcollege.ca <wwong174@my.centennialcollege.ca>
2022-10-23 19:42:16 -0400

    Connect to Mongodb Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 //db.js Wai Tung Wong 301222578 2022-10-21
-
+//mongodb User pw: nhX1QjFnbgDGM1gq
 module.exports =
  {
- "URI":"mongodb://127.0.0.1:27017/portfolioWeb"
+ //"URI":"mongodb://127.0.0.1:27017/portfolioWeb"
+ "URI":"mongodb+srv://WWT:nhX1QjFnbgDGM1gq@mongodbserver.fchd7ma.mongodb.net/?retryWrites=true&w=majority"
 }
